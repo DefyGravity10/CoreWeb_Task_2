71ca822d77e62be8442fef88b81b55c7c78735d1
KSchandanKumar <kschandankumar2001@gmail.com>
2020-08-25 16:10:04 +0530

    Added base html, css and js files for website structure

---
+++ quiz.js
@@ -0,0 +1,8 @@
+document.getElementById("page2").style.display="none";
+
+function start()
+{
+    document.getElementById("page1").style.display="none";
+    document.getElementById("title").style.display="none";
+    document.getElementById("page2").style.display="block";
+}
